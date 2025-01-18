0f073be37fc194ae2b539ffaea3aa9edf67915f0
codesyariah122 <pujiermanto@gmail.com>
2025-01-18 11:52:09 +0700

    fix/content

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -97,9 +97,7 @@ const about = {
     title: "Introduction",
     description: (
       <>
-        Selene is a Jakarta-based design engineer with a passion for transforming complex challenges
-        into simple, elegant design solutions. Her work spans digital interfaces, interactive
-        experiences, and the convergence of design and technology.
+        Great ideas begin with a spark, and at Mindsparks, we make sure it burns bright.
       </>
     ),
   },
